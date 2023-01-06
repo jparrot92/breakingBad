ded85a0e4fb616ff6f940806659fc7f26f9c657a
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-01-06 19:41:45 +0100

    Estructura de directorios recomendada

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
-<script lang="ts" src="@/App.ts"></script>
 
 <template>
   <div class="wrapper">
-    <h1>Counter: {{ counter }}</h1>
-    <button @click="counter++">+1</button>
+    <h1>App.vue</h1>
   </div>
 </template>
 
---
+++ src/characters/layout/CharacterLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>Character Layout</h1>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
