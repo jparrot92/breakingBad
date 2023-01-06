9f96488f03f1ee42fd528a2ab36ca63f4e75c3eb
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-01-06 23:23:54 +0100

    Router principal

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import AboutPage from '@/shared/pages/AboutPage.vue';
+import HomePage from '@/shared/pages/HomePage.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory( import.meta.env.BASE_URL ),
+    routes: [
+        // Public
+        { path: '/', name: 'home', component: HomePage },
+        { path: '/', name: 'home', component: AboutPage },
+
+        // Characters
+        {
+            path: '/characters',
+            name: 'characters',
+            component: () => import('@/characters/layout/CharacterLayout.vue')
+        },
+
+        // Default
+        { path: '/:pathMatch(.*)*', redirect: () => ({ name: 'home' }) }
+    ]
+});
+
+export default router;
