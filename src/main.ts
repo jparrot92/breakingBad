c1b3d757f25e6ab9cb87f7b4ab307259183870a4
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-01-18 17:58:36 +0100

    Vue TanStack Query

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 import './assets/main.css'
 
 const app = createApp(App)
+
+app.use(VueQueryPlugin)
 
 app.use(router)
 
