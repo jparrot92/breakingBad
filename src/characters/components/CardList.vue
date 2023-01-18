74e9710cddb848b7e45976c065ef10b62cae29f3
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-01-18 21:11:41 +0100

    useQuery

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -3,17 +3,31 @@ import { ref } from 'vue';
 import rickAndMortyApi from '@/api/rickAndMortyApi';
 import type { ResponseData, Character } from '@/characters/interfaces/character';
 import { useCharacters } from '../composables/useCharacters';
+import { useQuery } from '@tanstack/vue-query';
 
+//! 1- Normal suspense
 // const { results: characters } = (await rickAndMortyApi.get<ResponseData>('/character')).data
 // const characters = ref<Character[]>(data)
 
-const { isLoading, characters, hasError, errorMessage } = useCharacters()
+//! 2- Composable functions
+// const { isLoading, characters, hasError, errorMessage } = useCharacters()
+
+//! 3- TanStack Query
+const getCharactersSlow = async():Promise<Character[]> => {
+    const results = await rickAndMortyApi.get<ResponseData>('/character')
+    return results.data.results
+}
+
+const { isLoading, isError, data: characters, error } = useQuery({
+    queryKey: ['characters'],
+    queryFn: getCharactersSlow,
+})
 
 </script>
 
 <template>
     <h1 v-if="isLoading">Loading...</h1>
-    <h1 v-if="hasError">{{ errorMessage }}</h1>
+    <h1 v-if="isError">{{ error }}</h1>
     <ul>
         <li
             v-for="{ id, name } of characters"
