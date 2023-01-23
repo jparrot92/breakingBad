ef3b4d44df55e3880bce222ef2a0ae1666201433
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-01-23 22:31:03 +0100

    Personaje por ID

--- src/characters/pages/CharacterId.vue
+++ src/characters/pages/CharacterId.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+
+const route = useRoute();
+
+const { id } = route.params as { id: string }
 
 </script>
 
 <template>
     <div>
-        <h1>CharacterId</h1>
+        <h1>Character #{{ id }}</h1>
     </div>
 </template>
 
--- src/characters/router/index.ts
+++ src/characters/router/index.ts
@@ -5,28 +5,30 @@ import CharacterId from "@/characters/pages/CharacterId.vue";
 import CharacterList from "@/characters/pages/CharacterList.vue";
 import CharacterSearch from "@/characters/pages/CharacterSearch.vue";
 
+const ROUTE_NAME = 'characters';
+
 export const characterRoute: RouteRecordRaw = {
-    path: '/characters',
-    redirect: '/characters/list',
+    path: `/${ ROUTE_NAME }`,
+    redirect: `/${ ROUTE_NAME }/list`,
     component: CharacterLayout,
     children: [
-        {
-            path: 'by/id',
-            name: 'character-id',
-            props: { title: 'Por Id', visible: false },
-            component: CharacterId 
+        { 
+            path: 'by/:id',
+            name: 'character-id', 
+            props: { title: 'Por Id', visible: false, },
+            component: CharacterId,
         },
-        {
-            path: 'list',
-            name: 'character-list',
-            props: { title: 'Lista completa', visible: true },
-            component: CharacterList 
+        { 
+            path: `/${ ROUTE_NAME }/list`,
+            name: 'character-list', 
+            props: { title: 'Lista', visible: true, },
+            component: CharacterList,
         },
-        {
-            path: 'search',
-            name: 'character-search',
-            props: { title: 'Búsqueda', visible: true },
-            component: CharacterSearch 
+        { 
+            path: `/${ ROUTE_NAME }/search`,
+            name: 'character-search', 
+            props: { title: 'Búsqueda', visible: true, },
+            component: CharacterSearch,
         },
     ]
 }
