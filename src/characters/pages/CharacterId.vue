90d4efe52e9f355d33e260c10919ab872e58e05e
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-01-24 20:24:25 +0100

    Mostrar información del personaje

--- src/characters/pages/CharacterId.vue
+++ src/characters/pages/CharacterId.vue
@@ -15,12 +15,12 @@ const getCharacterCacheFirst = async( characterId: string ):Promise<Character> =
     if ( characterStore.checkIdInStore(characterId) ) {
         return characterStore.ids.list[characterId];
     }
-    const { data } = await rickAndMortyApi.get<Character>(`/characters/${ characterId }`);
+    const { data } = await rickAndMortyApi.get<Character>(`/character/${ characterId }`);
     return data;
 }
 
 const { data: character } = useQuery({
-    queryKey: ['characters', id],
+    queryKey: ['character', id],
     queryFn: () => getCharacterCacheFirst(id),
     onSuccess(character){
         characterStore.loadedCharacter(character);
@@ -30,12 +30,34 @@ const { data: character } = useQuery({
 </script>
 
 <template>
-    <div>
-        <h1>Character #{{ id }}</h1>
+    <h1 v-if="!character">Loading...</h1>
+
+    <div v-else>
+        <h1>{{ character.name }}</h1>
+        <div class="character-container">
+            <img :src="character.image" :alt="character.name">
+            <ul>
+                <li>Especie: {{ character.species }}</li>
+                <li>Tipo: {{ character.type }}</li>
+                <li>Genero: {{ character.gender }}</li>
+                <li>Origen: {{ character.origin.name }}</li>
+                <li>Localización: {{ character.location.name }}</li>
+                <li>Estado: {{ character.status }}</li>
+                <li>Epidodios: {{ character.episode.join(', ') }}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 
 <style scoped>
-
+.character-container {
+    display: flex;
+    flex-direction: row;
+}
+img {
+    width: 150px;
+    height: 150px;
+    border-radius: 5px;
+}
 </style>
