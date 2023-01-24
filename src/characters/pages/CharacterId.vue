a1ae7a3df5ea792e510c06f9db5533531de6407a
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-01-24 20:12:58 +0100

    Cargar información del personaje

--- src/characters/pages/CharacterId.vue
+++ src/characters/pages/CharacterId.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
+import { useQuery } from '@tanstack/vue-query';
+
+import rickAndMortyApi from '@/api/rickAndMortyApi';
+import characterStore from '@/store/characters.store';
+import type { Character } from '../interfaces/character';
 
 
 const route = useRoute();
 
 const { id } = route.params as { id: string }
+
+const getCharacterCacheFirst = async( characterId: string ):Promise<Character> => {
+    if ( characterStore.checkIdInStore(characterId) ) {
+        return characterStore.ids.list[characterId];
+    }
+    const { data } = await rickAndMortyApi.get<Character>(`/characters/${ characterId }`);
+    return data;
+}
+
+const { data: character } = useQuery({
+    queryKey: ['characters', id],
+    queryFn: () => getCharacterCacheFirst(id),
+    onSuccess(character){
+        characterStore.loadedCharacter(character);
+    },
+})
 
 </script>
 
